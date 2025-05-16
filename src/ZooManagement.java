6b859ef3a356479f64079b8006544cb4101e657b
s33721 <s33721@pjwstk.edu.pl>
2025-05-16 16:39:42 +0200

    Finished the task

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,2 +1,19 @@
 public class ZooManagement {
+    public static void main(String[] args) {
+        Animal lion = new Mammal("Lion", 8, 150);
+        Animal eagle = new Bird("Eagle", 10, 1000);
+        Animal crocodile = new Reptile("Crocodile", 4, "Green");
+        lion.move();
+        System.out.println("===============");
+        eagle.makeSound();
+        System.out.println("===============");
+        crocodile.move();
+        System.out.println("===============");
+        lion.displayInformation();
+        System.out.println("===============");
+        eagle.displayInformation();
+        System.out.println("===============");
+        crocodile.displayInformation();
+
+    }
 }

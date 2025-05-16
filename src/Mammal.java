22115148a8657b0f0d0c15d44d0050b6224602ff
s33721 <s33721@pjwstk.edu.pl>
2025-05-16 16:14:50 +0200

    Overridden methods and created a constructor in the Mammal class

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,2 +1,19 @@
-public class Mammal {
+public class Mammal extends Animal {
+
+
+    public Mammal(String name, int age) {
+        super(name, age);
+
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Mammal is making a sound");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Mammal is moving");
+    }
+
 }

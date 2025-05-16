7a49a7693629fa7f46666de4d068e10c08cc4eb5
s33721 <s33721@pjwstk.edu.pl>
2025-05-16 16:23:09 +0200

    Finished the bird class

--- src/Bird.java
+++ src/Bird.java
@@ -1,2 +1,29 @@
-public class Bird {
+public class Bird extends Animal {
+    private int featherCount;
+
+    public Bird(String name, int age, int featherCount) {
+        super(name, age);
+        this.featherCount = featherCount;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Bird is making a sound");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Bird is moving");
+    }
+
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+
+    @Override
+    public void displayInformation() {
+        super.displayInformation();
+        System.out.println("The feather count is " + featherCount);
+    }
+
 }

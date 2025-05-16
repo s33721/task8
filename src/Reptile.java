76de8c7120315af899b378e07558971dcf7d4101
s33721 <s33721@pjwstk.edu.pl>
2025-05-16 16:30:05 +0200

    Finished the reptile class

--- src/Reptile.java
+++ src/Reptile.java
@@ -1,2 +1,28 @@
-public class Reptile {
+public class Reptile extends Animal {
+    private String color;
+
+    public Reptile(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Reptile is making sound");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Reptile is moving");
+    }
+
+    public void spin() {
+        System.out.println("Reptile is spinning");
+    }
+
+    @Override
+    public void displayInformation() {
+        super.displayInformation();
+        System.out.println("Reptile color: " + color);
+    }
 }

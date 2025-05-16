e1fd0dd1a2dee43bec23b9840a6a0c812e5e5746
s33721 <s33721@pjwstk.edu.pl>
2025-05-16 16:08:37 +0200

    Wrote fields, constructor and getters for Animal class

---
+++ src/Animal.java
@@ -0,0 +1,18 @@
+public abstract class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
